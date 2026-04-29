2eb8d3900ed2df314858b39d01492ec39af6db8d
niksa1515 <shmoylova2006@mail.ru>
2026-04-29 15:38:34 +0500

    userpage и homepage

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // Tailwind v4 подключается через Vite-плагин
+  ],
+  server: {
+    host: '0.0.0.0', // нужно для Docker
+    port: 5173,
+  },
 })
